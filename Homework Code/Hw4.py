910e9c39d816615d8624e56d4918aec3e66b5360
YuWVandy <yu.wang.1@Vanderbilt.Edu.Github>
2020-02-06 21:19:42 -0600

    final HW4

--- Homework Code/Hw4.py
+++ Homework Code/Hw4.py
@@ -146,13 +146,14 @@ P_mean = [25000, 20000, 28000, 22000, 0.07]
 E1, E2, E3, E4, Noise = [], [], [], [], []
 klE1, klE2, klE3, klE4, klnoi = [], [], [], [], []
 
-#Generate Observe value
+#Generate Observed value
 P = 2000
 obs = np.zeros(5)
 for i in range(5):
     data, Sprob = DataSample(S_sigma, S_mean)
     obs[i] = PhyModel(data, P)
 
+##Sampling
 Temp = 0
 while(Temp <= 200):
     temp = 0
@@ -193,7 +194,7 @@ while(Temp <= 200):
 
     Temp += 1
 
-#Plot the kl value
+#Plot the KL value to see whether converge
 X = np.arange(0, len(klE1), 1)
 fig3 = plt.figure()
 plt.plot(X, klE1/np.max(klE1), label = 'E1', color = 'red', marker = 'o', markersize=0.3)
@@ -209,7 +210,7 @@ plt.grid(True)
 #plt.savefig("InterStrength2.png", dpi = 2000, bbox_inches='tight') 
 plt.show()
 
-
+##Generate the samples from the prior distribution so that we can plot and compare
 psE1, psE2, psE3, psE4, psnoi = [], [], [], [], []
 for i in range(len(E1)):
     psE1.append(np.random.normal(P_mean[0], P_sigma[0]))
@@ -226,7 +227,7 @@ for i in range(len(E4)):
 for i in range(len(Noise)):
     psnoi.append(np.random.normal(P_mean[4], P_sigma[4]))
     
-#Plot the Prior Distribution(Sampling Distribution)  
+#Plot the Prior Distribution and the final posterior distribution 
 PlotDistri2(psE1, E1, 'red', 'blue', 'The value of the variable E1', 'E1_Prior', 'E1', 'the Frequency', 0.4, 0.3)
 PlotDistri2(psE2, E2, 'red', 'blue', 'The value of the variable E2', 'E2_Prior', 'E2', 'the Frequency', 0.4, 0.3)
 PlotDistri2(psE3, E3, 'red', 'blue', 'The value of the variable E3', 'E3_Prior', 'E3', 'the Frequency', 0.4, 0.3)
